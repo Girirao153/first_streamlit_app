5869de59bcb792c6a4160655ac4db8150024d5c0
Girirao153 <148484215+Girirao153@users.noreply.github.com>
2023-10-20 11:22:51 -0700

    Update streamlit_app.py
    Show data in a nicer way - capitalize first letters of fruit names and show them in a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Capitalize the first letters in the fruit names. 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show the fruit information in a dataframe.
+streamlit.dataframe(fruityvice_normalized)
